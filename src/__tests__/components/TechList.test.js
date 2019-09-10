51874c5bd801745cd5d933b630bfdffe4e17a04e
andrefangeloni <andrefangeloni@gmail.com>
2019-09-09 22:42:45 -0300

    Primeiro teste usando formulário

---
+++ src/__tests__/components/TechList.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import TechList from "~/components/TechList";
+
+describe("TechList component", () => {
+  it("should be able to add new tech", () => {
+    const { getByText, getByTestId, getByLabelText } = render(<TechList />);
+
+    fireEvent.change(getByLabelText("Tech"), { target: { value: "Node.js" } });
+    fireEvent.submit(getByTestId("tech-form"));
+
+    expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
+    expect(getByLabelText("Tech")).toHaveValue("");
+  });
+});
---
+++ src/components/TechList/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+// import { Container } from './styles';
+
+export default function TechList() {
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState("");
+
+  function handleAddTech() {
+    setTechs([...techs, newTech]);
+    setNewTech("");
+  }
+
+  return (
+    <form data-testid="tech-form" onSubmit={handleAddTech}>
+      <ul data-testid="tech-list">
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+
+      <label htmlFor="tech">Tech</label>
+      <input
+        id="tech"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+      />
+
+      <button onClick={handleAddTech}>Adicionar</button>
+    </form>
+  );
+}
