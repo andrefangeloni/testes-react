bc83346cecdfa08b179c1b85f87b1ae1f566dd6f
andrefangeloni <andrefangeloni@gmail.com>
2019-09-25 22:42:36 -0300

    Tests done

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -1,35 +1,36 @@
 import React from "react";
-import { render, fireEvent, cleanup } from "@testing-library/react";
+import { useSelector, useDispatch } from "react-redux";
+import { render, fireEvent } from "@testing-library/react";
+
+import { addTech } from "~/store/modules/techs/actions";
+
 import TechList from "~/components/TechList";
 
+jest.mock("react-redux");
+
 describe("TechList component", () => {
-  beforeEach(() => {
-    localStorage.clear();
+  it("should render tech list", () => {
+    useSelector.mockImplementation(cb =>
+      cb({
+        techs: ["Node.js", "ReactJS"]
+      })
+    );
+
+    const { getByTestId, getByText } = render(<TechList />);
+
+    expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
+    expect(getByTestId("tech-list")).toContainElement(getByText("ReactJS"));
   });
 
   it("should be able to add new tech", () => {
-    const { getByText, getByTestId, getByLabelText } = render(<TechList />);
+    const { getByTestId, getByLabelText } = render(<TechList />);
+
+    const dispatch = jest.fn();
+    useDispatch.mockReturnValue(dispatch);
 
     fireEvent.change(getByLabelText("Tech"), { target: { value: "Node.js" } });
     fireEvent.submit(getByTestId("tech-form"));
 
-    expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
-    expect(getByLabelText("Tech")).toHaveValue("");
-  });
-
-  it("should store techs in storage", () => {
-    let { getByText, getByTestId, getByLabelText } = render(<TechList />);
-
-    fireEvent.change(getByLabelText("Tech"), { target: { value: "Node.js" } });
-    fireEvent.submit(getByTestId("tech-form"));
-
-    cleanup();
-
-    ({ getByText, getByTestId, getByLabelText } = render(<TechList />));
-    expect(localStorage.setItem).toHaveBeenCalledWith(
-      "techs",
-      JSON.stringify(["Node.js"])
-    );
-    expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
+    expect(dispatch).toHaveBeenCalledWith(addTech("Node.js"));
   });
 });
